5e46dc2369613ffef4548023fbce2cc990d5064b
dworker <dworker863@gmail.com>
2023-01-07 20:45:28 +0600

    Changed header bg image

--- components/Blocks/Container/StyledContainer.ts
+++ components/Blocks/Container/StyledContainer.ts
@@ -3,6 +3,28 @@ import styled from 'styled-components';
 export const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 1440px;
+  width: 1320px;
   margin: 0 auto;
+
+  @media (max-width: 1400px) {
+    width: 1140px;
+  }
+
+  @media (max-width: 1200px) {
+    width: 960px;
+  }
+
+  @media (max-width: 992px) {
+    width: 720px;
+  }
+
+  @media (max-width: 768px) {
+    width: 540px;
+  }
+
+  @media (max-width: 576px) {
+    width: 100%;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
