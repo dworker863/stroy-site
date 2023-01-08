773c5af754ef58803745d76580523e69f7f97a44
dworker <dworker863@gmail.com>
2023-01-08 14:36:20 +0600

    Header info block all screens

--- components/Sections/Header/StyledHeader.ts
+++ components/Sections/Header/StyledHeader.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const StyledHeader = styled.header`
   position: relative;
+  display: flex;
+  /* align-items: center; */
   height: 100vh;
   background-image: url('images/header-bg.png');
   background-repeat: no-repeat;
@@ -14,16 +16,45 @@ export const StyledHeader = styled.header`
 `;
 
 export const StyledHeaderInfo = styled.div`
+  align-self: center;
   width: 40%;
-  align-self: flex-start;
-  justify-self: flex-end;
   margin-left: auto;
-  transform: translateY(50%);
+  color: ${({ theme }) => theme.colors.fontColor};
+
+  @media (max-width: 1400px) {
+  }
+
+  @media (max-width: 1200px) {
+  }
+
+  @media (max-width: 992px) {
+    width: 60%;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+  @media (max-width: 576px) {
+    font-size: 14px;
+  }
 `;
 
 export const StyledHeaderTitle = styled.h1`
   margin-bottom: 20px;
-  font-size: 48px;
+  font-size: 42px;
+
+  @media (max-width: 1400px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 1200px) {
+    font-size: 30px;
+  }
+
+  @media (max-width: 576px) {
+    font-size: 26px;
+  }
 `;
 
 export const StyledHeaderDesc = styled.p``;
--- components/commonStyles/theme.ts
+++ components/commonStyles/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from 'styled-components';
 
 export const defaultTheme: DefaultTheme = {
   colors: {
-    fontColor: '#262121db',
+    fontColor: '#262121eb',
     primary: '#b20a4ec7',
     secondary: '#458FFD',
   },
