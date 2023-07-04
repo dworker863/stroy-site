c2a01865c1dbde12334a4ea302465bdbdc7deebd
dworker <dworker863@gmail.com>
2023-07-04 08:42:16 +0600

    NotFoundPage all screens

--- pages/404/StyledNotFoundPage.ts
+++ pages/404/StyledNotFoundPage.ts
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 export const StyledNotFoundPage = styled.div`
@@ -12,14 +13,45 @@ export const StyledNotFoundPage = styled.div`
 export const StyledNotFoundCode = styled.div`
   display: flex;
   align-items: center;
-  font-size: 192px;
+  font-size: 154px;
 `;
 
 export const StyledNotFoundTitle = styled.h2`
   width: 300px;
+  margin-bottom: 20px;
   text-align: center;
 `;
 export const StyledNotFoundDesc = styled.p`
   width: 300px;
   text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const StyledNotFoundNav = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const StyledNotFoundLink = styled(Link)`
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.secondary};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export const StyledNotFoundSeparator = styled.span`
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.fontColor};
+
+  @media (max-width: 576px) {
+    display: none;
+  }
 `;
--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useEffect } from 'react';
 import {
   StyledNotFoundCode,
   StyledNotFoundDesc,
+  StyledNotFoundLink,
+  StyledNotFoundNav,
   StyledNotFoundPage,
+  StyledNotFoundSeparator,
   StyledNotFoundTitle,
 } from './StyledNotFoundPage';
 
@@ -29,16 +33,42 @@ const NotFoundPage: NextPage = () => {
         <Image
           src="/images/builder.avif"
           alt="Разбитая стена"
-          width={150}
-          height={150 / ratio}
+          width={140}
+          height={140 / ratio}
         />
         4
       </StyledNotFoundCode>
-      <StyledNotFoundTitle>Страница не найдена</StyledNotFoundTitle>;
+      <StyledNotFoundTitle>Страница не найдена</StyledNotFoundTitle>
       <StyledNotFoundDesc>
         Возможно, она была удалена или ее никогда не существовало.
       </StyledNotFoundDesc>
-      ;
+      <StyledNotFoundNav>
+        <li>
+          <StyledNotFoundLink href="/">
+            Главная <StyledNotFoundSeparator>|</StyledNotFoundSeparator>
+          </StyledNotFoundLink>
+        </li>
+        <li>
+          <StyledNotFoundLink href="/projects">
+            Проекты <StyledNotFoundSeparator>|</StyledNotFoundSeparator>
+          </StyledNotFoundLink>
+        </li>
+        <li>
+          <StyledNotFoundLink href="/video">
+            Видео <StyledNotFoundSeparator>|</StyledNotFoundSeparator>
+          </StyledNotFoundLink>
+        </li>
+        <li>
+          <StyledNotFoundLink href="/calculator">
+            Калькулятор <StyledNotFoundSeparator>|</StyledNotFoundSeparator>
+          </StyledNotFoundLink>
+        </li>
+        <li>
+          <StyledNotFoundLink href="/contacts">
+            Контакты <StyledNotFoundSeparator>|</StyledNotFoundSeparator>
+          </StyledNotFoundLink>
+        </li>
+      </StyledNotFoundNav>
     </StyledNotFoundPage>
   );
 };
